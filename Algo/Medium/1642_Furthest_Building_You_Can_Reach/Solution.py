791c8d7811201bc043055b6c781c5c892507f946
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2022-06-22 23:19:49 -0700

    some smol edits

--- Algo/Medium/1642_Furthest_Building_You_Can_Reach/Solution.py
+++ Algo/Medium/1642_Furthest_Building_You_Can_Reach/Solution.py
@@ -1,19 +1,14 @@
 from heapq import *
 from typing import *
 
-## 2022-06-22 
+## 2022-06-22 (max heap approach)
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:  
         bricks_used = []
         remaining_bricks = bricks
         remaining_ladders = ladders
-        for i in range(len(heights)):
-            if i == len(heights)-1:
-                return i
-            
+        for i in range(len(heights)-1):           
             height_diff = heights[i+1] - heights[i]
-
-            ## if next building is the same height or lower, we can just continue
             if height_diff <= 0:
                 continue
                 
@@ -21,8 +16,6 @@ class Solution:
                 remaining_bricks -= height_diff
                 heappush(bricks_used, -height_diff)
             else:
-                ## if using a ladder previously would have saved more bricks, then lose a ladder and
-                ## gain back the difference
                 if bricks_used and height_diff < -bricks_used[0] and remaining_ladders > 0:
                     remaining_bricks += -heappop(bricks_used) - height_diff
                     remaining_ladders -= 1
@@ -31,5 +24,5 @@ class Solution:
                     remaining_ladders -= 1
                 else:
                     return i
-
-        return -1
+                
+        return len(heights)-1
