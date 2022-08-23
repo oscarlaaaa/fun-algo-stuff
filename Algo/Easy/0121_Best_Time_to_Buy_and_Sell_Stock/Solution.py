1c2fb22336954b52e5ef5564a984d150b33ae2b0
Oscar <oscarla5747@gmail.com>
2022-08-23 00:04:56 -0700

    z

--- Algo/Easy/0121_Best_Time_to_Buy_and_Sell_Stock/Solution.py
+++ Algo/Easy/0121_Best_Time_to_Buy_and_Sell_Stock/Solution.py
@@ -11,3 +11,19 @@ class Solution:
             max_profit = max(max_profit, prices[i] - current_stock)
         
         return max_profit
+
+## 2022-08-22
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 1:
+            return 0
+        
+        profit = 0
+        cheapest = prices[0]
+        for i in range(1, len(prices)):
+            profit = max(profit, prices[i] - cheapest)
+            if prices[i] < cheapest:
+                cheapest = prices[i]
+        
+        return profit
+        
