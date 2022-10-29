40b8dd5617e985a3bd86aa370845ff64589a2321
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2022-10-29 16:44:39 -0700

    old daily

--- Algo/Medium/0049_Group_Anagrams/Solution.py
+++ Algo/Medium/0049_Group_Anagrams/Solution.py
@@ -12,4 +12,21 @@ class Solution:
                 anagrams[sorted_s] = [i]
         
         return [[strs[i] for i in v] for v in anagrams.values()]
-        
+
+## 2022-10-29 (hashing with tuples)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def build_tuple(s):
+            output = [0 for _ in range(26)]
+            a = ord('a')
+            for c in s:
+                output[ord(c) - a] += 1
+            return tuple(output)
+            
+        groups = dict()
+        for s in strs:
+            t = build_tuple(s)
+            if t not in groups:
+                groups[t] = []
+            groups[t].append(s)
+        return [anagrams for anagrams in groups.values()]
