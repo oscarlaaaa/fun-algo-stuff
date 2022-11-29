ca1151fb4ae9bea6e89d49fa7a4ad26b7f288dac
Oscar <oscarla5747@gmail.com>
2022-11-29 00:42:15 -0800

    lmao

---
+++ Algo/Medium/2396_Strictly_Palindrome_Number/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        /*
+            I tried many combinations of numbers up to 17. and it always seemed
+            more and more impossible to get a symmetrical number as n increases.
+            As such, I assumed that this was just a trick question and returned false.
+        */
+        return false;
+    }
+};
